1844bec9654bedac19bd68ddeb67db9cd669cf87
june <86581908+jxne00@users.noreply.github.com>
2023-10-09 11:58:13 +0800

    optimise code

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,32 @@ import moonIcon from '../assets/images/moon-2287.svg';
 function Header() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
+  // set diff text color based on theme
+  const textColor = theme === 'dark' ? 'text-slate-400' : 'text-blue-800';
+
+  // set diff icon based on theme
+  const iconType =
+    theme === 'light'
+      ? { src: moonIcon, alt: 'moon icon' }
+      : { src: sunIcon, alt: 'sun icon' };
+
   return (
     <header className='bg-transparent absolute top-0 left-0 w-full flex items-center justify-between p-4 z-10'>
-      <span
-        className={`italic font-bold text-lg ${
-          theme === 'dark' ? 'text-slate-400' : 'text-blue-800'
-        }`}>
+      <span className={`italic font-bold text-lg ${textColor}`}>
         June's Portfolio
       </span>
 
       {/* change the theme */}
-      <div className='flex items-center' onClick={toggleTheme}>
-        {theme === 'light' ? (
-          <img src={moonIcon} className='w-5 h-5' alt='moon icon' />
-        ) : (
-          <img src={sunIcon} className='w-5 h-5' alt='sun icon' />
-        )}
-      </div>
+      <button
+        className='flex items-center p-2 focus:outline-none focus:ring'
+        onClick={toggleTheme}
+        aria-label='Theme toggle button'>
+        <img
+          src={iconType.src}
+          className='w-5 h-5 transform transition-transform duration-300 hover:scale-125'
+          alt={iconType.alt}
+        />
+      </button>
     </header>
   );
 }
